ab1cc711573076d57aadd0698a56356b5cc3a52d
guicaixeta <caixetag@gmail.com>
2017-12-03 21:59:20 -0200

    ListItem component - index
    ListItem component - index

--- client/src/components/Lists/ListItem/index.jsx
+++ client/src/components/Lists/ListItem/index.jsx
@@ -1,1 +1,20 @@
-export { default } from './ListItem';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+
+import ListItem from './ListItem';
+import { editList } from '../../../modules/list';
+import { collapseLeftSideBar } from '../../../modules/app';
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      collapseLeftSideBar,
+      editList
+    },
+    dispatch
+  );
+
+export default withRouter(
+  connect(() => ({}), mapDispatchToProps)(ListItem)
+);
