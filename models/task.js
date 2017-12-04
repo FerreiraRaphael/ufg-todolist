50bfc35337d5e18461d1379671a0034d84670ba2
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-12-04 14:28:09 -0200

    add archived and finishDate to Tasks and Lists

--- models/task.js
+++ models/task.js
@@ -1,7 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     title: DataTypes.STRING,
-    done: DataTypes.BOOLEAN
+    done: DataTypes.BOOLEAN,
+    archived: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    finishDate: DataTypes.DATE
   });
 
   Task.associate = ({ List }) => {
